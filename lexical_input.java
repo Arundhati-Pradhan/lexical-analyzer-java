97a722785b057ba2097c260124df9189b5b6d996
Arundhati-Pradhan <91604775+Arundhati-Pradhan@users.noreply.github.com>
2022-04-28 00:22:48 +0530

    Create lexical_input.java

---
+++ lexical_input.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main ( String [ ] args ) {
+        int a = 10 ;
+        float c = 67 ;
+        System.out.println ( "Hello, World!" ) ;
+        float sum = a + c ;
+    }
+}
